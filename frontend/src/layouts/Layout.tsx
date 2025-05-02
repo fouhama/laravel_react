7200d2ab2a1d55bcd168c9ced5ba88d133c3a931
tariq <elfouhamatariq@gmail.com>
2025-05-02 01:08:05 +0100

    layout and page login

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -4,7 +4,18 @@ const Layout = () => {
   return (
     <>
     <header>
-        header
+        <nav className="flex justify-between items-center bg-gray-800 text-white p-4">
+            <div className="text-2xl font-bold">My App</div>
+            <ul className="flex space-x-4">
+                <li><a href="/" className="hover:text-gray-400">Home</a></li>
+                <li><a href="/users" className="hover:text-gray-400">Users</a></li>
+                <li><a href="/login" className="hover:text-gray-400">Login</a></li>
+                <li><a href="/register" className="hover:text-gray-400">Register</a></li>
+                <li><a href="/about" className="hover:text-gray-400">About</a></li>
+                <li><a href="/contact" className="hover:text-gray-400">Contact</a></li>
+            </ul>
+            
+        </nav>
     </header>
         <Outlet/>
     <footer>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -4,6 +4,7 @@ import {z} from "zod"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage  } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { axiosClient } from "@/api/axios"
 const formShecma = z.object({
   username : z.string().min(2,{
     message: "Username must be greater than 2 characters."
@@ -19,12 +20,19 @@ const Login = () => {
       password: ''
     }
   }) 
-  const submit = (values : z.infer<typeof formShecma>)=>{
-    console.log(values);
+const  submit = async(values : z.infer<typeof formShecma>)=>{
+    await axiosClient.post('/login', values).then((res)=>{
+      console.log(res.data)
+    }
+    ).catch((err)=>{
+      console.log(err)
+    }
+    )
+    
     
   }
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-screen ">
         <div className="border border-gray-300 rounded-2xl w-2xl p-5 shadow-sm">
           <h1 className="text-3xl text-gray-500 bold mb-3 text-center">Autentifiaction</h1>
           <Form {...form}>
@@ -35,26 +43,26 @@ const Login = () => {
                   <FormItem>
                     <FormLabel> Username</FormLabel>
                     <FormControl>
-                       <Input placeholder="Username" {...field} className="focus:border-none focus:outline-none"/>
+                       <Input placeholder="Username" {...field} className="bg-white focus:border-none focus:outline-none"/>
                     </FormControl>
                     <FormMessage className="text-red-500"/>
                   </FormItem>
                 )}
               
               />
-              <FormField   name="password" render={({field})=>(
+              <FormField    name="password" render={({field})=>(
                 <FormItem>
                   <FormLabel>
                     Password
                   </FormLabel>
                   <FormControl>
-                    <Input placeholder="Password" {...field}  className="focus:border-none focus:outline-none" type="password"/>
+                    <Input placeholder="Password" {...field}  className=" bg-white focus:border-none focus:outline-none" type="password"/>
                   </FormControl>
                   <FormMessage className="text-red-500"/>
                 </FormItem>
               )}/>
 
-              <Button type="submit">Submit</Button>
+              <Button type="submit" className="float-end w-40 cursor-pointer">Submit</Button>
               </form>
           </Form>
         </div>
